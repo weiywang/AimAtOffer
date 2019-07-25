6056189337ab0d7d8cf0fd945a12ebca95b4e416
wangweiya <sa517370@mail.ustc.edu.cn>
2019-07-25 22:31:54 +0800

    ReplaceSpace completed 1.0

---
+++ src/main/java/Find.java
@@ -0,0 +1,20 @@
+package main.java;
+
+public class Find {
+    public boolean Find(int target, int [][] array) {
+        if(array.length == 0){
+            return false;
+        }
+        for(int i = 0, j = array[0].length - 1; i < array.length && j >= 0;){
+            if(array[i][j] < target){
+                i++;
+            }
+            else if(array[i][j] > target){
+                j--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/ReplaceSpace.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class ReplaceSpace {
+    public String replaceSpace(StringBuffer str) {
+        char[] chars = str.toString().toCharArray();
+        StringBuilder res = new StringBuilder();
+        for(char c: chars){
+            if(c == ' '){
+                res.append("%20");
+            }else{
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
