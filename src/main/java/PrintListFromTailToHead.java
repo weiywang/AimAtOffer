cf298c7bbde671be84c2be5fc4cb5baea7bd6cc4
wangweiya <sa517370@mail.ustc.edu.cn>
2019-07-26 14:36:26 +0800

    PrintListFromTailToHead completed 1.0

---
+++ src/main/java/PrintListFromTailToHead.java
@@ -0,0 +1,40 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class PrintListFromTailToHead {
+
+    // stack method
+//    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+//        LinkedList<Integer> stack = new LinkedList<>();
+//
+//        while(listNode != null){
+//            stack.addLast(listNode.val);
+//            listNode = listNode.next;
+//        }
+//
+//        ArrayList<Integer> res = new ArrayList<>();
+//
+//        while(!stack.isEmpty()){
+//            res.add(stack.pollLast());
+//        }
+//
+//        return res;
+//    }
+
+    // 递归方法
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+        print(res, listNode);
+        return res;
+    }
+
+    public void print(ArrayList<Integer> res, ListNode listNode){
+        if(listNode == null){
+            return;
+        }
+        print(res, listNode.next);
+        res.add(listNode.val);
+    }
+}
